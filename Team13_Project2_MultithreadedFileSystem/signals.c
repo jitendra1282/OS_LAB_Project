197c2ed1d7a4e6aae7ef5ffbadb84b50dd972f8c
nithyaharshininatti07-oss <nithyaharshininatti07@gmail.com>
2026-04-13 00:34:29 +0530

    Update signals.c

--- Team13_Project2_MultithreadedFileSystem/signals.c
+++ Team13_Project2_MultithreadedFileSystem/signals.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 
 void handle_sigint(int sig) {
+    (void)sig;
     printf("\n[Signal] Graceful shutdown initiated...\n");
     exit(0);
 }
 
 void handle_sigusr1(int sig) {
+    (void)sig;
     printf("\n[Status] System is idle/processing...\n");
 }
 
